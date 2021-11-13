f60fc0fcfa54a494ca679e85ed9bff39b52351cc
ssssujini99 <sssssjin99@naver.com>
2021-11-13 21:14:47 +0900

    모델 최종 수정

--- django-tutorial/mysite/app1/models.py
+++ django-tutorial/mysite/app1/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from pytz import timezone
 from django.contrib.auth.models import AbstractUser
 
 
@@ -28,10 +29,9 @@ class User(AbstractUser):
 # 학교 모델
 class School(models.Model):
     name_k = models.CharField(max_length=20) # 학교이름(한글)
-    name_e = models.CharField(max_length=20) # 학교이름(영문)
     location = models.CharField(max_length=100) # 주소
-    loc_x = models.FloatField() # api x 좌표
-    loc_y = models.FloatField() # api y 좌표
+    loc_x = models.FloatField() # x 좌표 (경도)
+    loc_y = models.FloatField() # y 좌표 (위도)
     restaurants = models.ManyToManyField('Restaurant', related_name='nearby_schools') # 식당모델과 manytomany 연결
 
     def __str__(self):
@@ -40,20 +40,26 @@ class School(models.Model):
 
 # 식당 모델
 class Restaurant(models.Model):
-    name = models.CharField(max_length=30) # 식당이름
-    controlNum = models.CharField(max_length=50) # 식당 관리번호
-    category = models.CharField(max_length=10, blank=True, null=True) # 식당 카테고리
-    likeCount = models.IntegerField(default=0) # 맛집이에요 수
-    address1 = models.CharField(max_length=200, blank=True, null=True) # 주소1
-    address2 = models.CharField(max_length=200, blank=True, null=True) # 주소2
-    loc_x = models.FloatField() # x좌표
-    loc_y = models.FloatField() # y좌표
+    name = models.CharField(max_length=30) # 식당이름(사업장명)
+    controlNum = models.CharField(max_length=50, blank=True, null=True) # 식당 관리번호
+    category = models.CharField(max_length=10, blank=True, null=True) # 식당 카테고리 (업태구분명)
+    address1 = models.CharField(max_length=200, blank=True, null=True) # 주소1 (지번주소)
+    address2 = models.CharField(max_length=200, blank=True, null=True) # 주소2 (도로명주소)
+    loc_x = models.FloatField() # x좌표(경도)
+    loc_y = models.FloatField() # y좌표(위도)
+    likeCount = models.IntegerField(default=0)  # 맛집이에요 수
 
     class Meta:
         ordering = ('-likeCount',) # 좋아요 역순으로 정렬
 
     def __str__(self):
         return self.name # 식당 이름을 대표값으로
+
+
+# 식당 - 맛집이에요
+class Res_like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey('Restaurant', on_delete=models.CASCADE)
 
 
 # 찜 모델
@@ -82,7 +88,13 @@ class Review(Base):
 
 
 # Review의 사진 모델
-#class Photo(models.Model):
-#    review = models.ForeignKey(Review, on_delete=models.CASCADE)
-#    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-#    image = models.ImageField(upload_to='images/', blank=True, null=True, max=8)
+class Photo(models.Model):
+   review = models.ForeignKey(Review, on_delete=models.CASCADE) # 해당 댓글
+   restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE) # 해당 식당
+   images = models.ImageField(upload_to='images/', blank=True, null=True) # 이미지들
+
+
+# 히스토리 모델
+class History(Base):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # 사용자
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE) # 조회한 식당
